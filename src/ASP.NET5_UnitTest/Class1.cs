f332cbd7d8c1d8989dcfc8a3e8be9517d5ac6a0e
Pranav Rastogi <pranavra@microsoft.com>
2015-07-28 11:55:03 -0700

    Initial checkin of the sample

---
+++ src/ASP.NET5_UnitTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASP.NET5_UnitTest
+{
+    public class Class1
+    {
+        [Fact]
+        public void UniTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
